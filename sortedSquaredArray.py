59b208973b35797c08f22c1a8cc5c6432dd1b15f
Kimberly Swinton <kimberlyswinton@gmail.com>
2021-10-24 12:02:50 -0400

    added pseudo code for sortedSquaredArray

--- sortedSquaredArray.py
+++ sortedSquaredArray.py
@@ -11,3 +11,27 @@ def sortedSquaredArray(array):
 	sortedArray.sort()
 	# return the new sorted array
 	return sortedArray
+
+# SOLUTION 2 -------------------------------------------------------------------------------
+
+# initiate a new array
+	
+	# assign 2 pointers for the end
+	
+	# and the beganning of the array
+	
+	# loop through the array using reversed() because  range() because it's in ascending
+	# order at lenth to get the entire length of the array
+	
+		# assign the pointers at the arrays index so that it can be compared
+		
+		# we're going to compare what's bigger to the absolute value of the smallerValue to the bigger value
+		
+			# then the sorted array at the index will be assigned to square the smallest value 
+			# then increment the starter pointer by one
+			
+		# otherwise 
+			# then the sorted array at the index will be assigned to square the largest value
+			# then increment the end pointer by minus 1
+			
+	# return the new sorted array
